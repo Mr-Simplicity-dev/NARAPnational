f13378013018302f3a615ae02212b133705c73a8
Abdulkareem Onimisi Moshood <moshood2uall@gmail.com>
2025-09-12 21:49:37 +0100

    make changes

--- public/member/register.php
+++ public/member/register.php
@@ -52,15 +52,42 @@
           <label class="form-label">State</label>
           <select name="state" class="form-control" required>
             <option value="" selected disabled>Select State</option>
-            <option>Abia</option><option>Adamawa</option><option>Akwa Ibom</option><option>Anambra</option>
-            <option>Bauchi</option><option>Bayelsa</option><option>Benue</option><option>Borno</option>
-            <option>Cross River</option><option>Delta</option><option>Ebonyi</option><option>Edo</option>
-            <option>Ekiti</option><option>Enugu</option><option>Gombe</option><option>Imo</option>
-            <option>Jigawa</option><option>Kaduna</option><option>Kano</option><option>Katsina</option>
-            <option>Kebbi</option><option>Kogi</option><option>Kwara</option><option>Lagos</option>
-            <option>Nasarawa</option><option>Niger</option><option>Ogun</option><option>Ondo</option>
-            <option>Osun</option><option>Oyo</option><option>Plateau</option><option>Rivers</option>
-            <option>Sokoto</option><option>Taraba</option><option>Yobe</option><option>Zamfara</option>
+            <option>Abia</option>
+            <option>Adamawa</option>
+            <option>Akwa Ibom</option>
+            <option>Anambra</option>
+            <option>Bauchi</option>
+            <option>Bayelsa</option>
+            <option>Benue</option>
+            <option>Borno</option>
+            <option>Cross River</option>
+            <option>Delta</option>
+            <option>Ebonyi</option>
+            <option>Edo</option>
+            <option>Ekiti</option>
+            <option>Enugu</option>
+            <option>Gombe</option>
+            <option>Imo</option>
+            <option>Jigawa</option>
+            <option>Kaduna</option>
+            <option>Kano</option>
+            <option>Katsina</option>
+            <option>Kebbi</option>
+            <option>Kogi</option>
+            <option>Kwara</option>
+            <option>Lagos</option>
+            <option>Nasarawa</option>
+            <option>Niger</option>
+            <option>Ogun</option>
+            <option>Ondo</option>
+            <option>Osun</option>
+            <option>Oyo</option>
+            <option>Plateau</option>
+            <option>Rivers</option>
+            <option>Sokoto</option>
+            <option>Taraba</option>
+            <option>Yobe</option>
+            <option>Zamfara</option>
             <option>FCT (Abuja)</option>
           </select>
         </div>
