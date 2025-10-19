bfb4b866bff3536bc69a16361b8681d3cf264c07
Abdulkareem Onimisi Moshood <moshood2uall@gmail.com>
2025-10-19 03:49:47 +0100

    make changes

--- public/blog.php
+++ public/blog.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blogs - NARAP</title>
+    <link rel="icon" type="image/png" href="/uploads/slider/Narap.png"/>
 
 <!-- Bootstrap CSS -->
 <link href="css/bootstrap.min.css" rel="stylesheet">
